30de07be7729d3bf5327db50aec6d27809028eb9
Duncan M. McGreggor <oubiwann@gmail.com>
2015-05-09 16:24:52 -0500

    Updated docs make targets.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -1,11 +1,19 @@
 .PHONY: docs
-REPO = git@github.com:oubiwann/xb.git
-DOCS_BUILD_DIR = docs/build
+REPO = $(shell git config --get remote.origin.url)
+DOCS_DIR = docs
+DOCS_BUILD_DIR = $(DOCS_DIR)/build
+SLATE_GIT_HACK = $(DOCS_DIR)/.git
 
-devdocs:
+$(SLATE_GIT_HACK):
+	cd $(DOCS_DIR) && ln -s ../.git .
+
+docs-setup:
+	cd docs && bundle install
+
+devdocs: $(SLATE_GIT_HACK)
 	cd docs && bundle exec middleman server
 
-docs:
+docs: $(SLATE_GIT_HACK)
 	cd docs && rake build
 
 publish: docs
@@ -14,4 +22,7 @@ publish: docs
 	git init && \
 	git add * &> /dev/null && \
 	git commit -a -m "Generated content." &> /dev/null && \
-	git push -f $(REPO) master:gh-pages
+	git push -f $(REPO) master:gh-pages && \
+	cd - && \
+	cd $(DOCS_DIR) && \
+	rm .git
